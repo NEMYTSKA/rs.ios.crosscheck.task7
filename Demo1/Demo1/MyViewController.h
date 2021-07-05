93e0d4d70798ce487b4a729935f48d51c2663690
ILLIA NEMYTSKA <78763502+NEMYTSKA@users.noreply.github.com>
2021-07-05 16:01:28 +0300

    fix: start project

---
+++ Demo1/Demo1/MyViewController.h
@@ -0,0 +1,17 @@
+//
+//  MyViewController.h
+//  Demo1
+//
+//  Created by ilia on 05/07/2021.
+//  Copyright © 2021 Victor Semenchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Demo1/Demo1/MyViewController.m
@@ -0,0 +1,32 @@
+//
+//  MyViewController.m
+//  Demo1
+//
+//  Created by ilia on 05/07/2021.
+//  Copyright © 2021 Victor Semenchuk. All rights reserved.
+//
+
+#import "MyViewController.h"
+
+@interface MyViewController ()
+
+@end
+
+@implementation MyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
